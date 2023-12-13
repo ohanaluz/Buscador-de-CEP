e15c33e1f14249652072383f43cc04ca78ec12f7
ohana.luz <hoodstxy@gmail.com>
2023-12-13 19:11:18 -0300

    Inicializar projeto React CEP Locator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import { SlMagicWand } from "react-icons/sl";
+import './styles.css';
+
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState ('');
+
+  const [cep, setCep] = useState ({})
+
+  async function handleSearch(){
+    //01001000/json/
+
+    if (input === ''){
+      alert("Preencha o CEP.")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+    } catch (error){
+      alert("Erro ao localizar o CEP.");
+      setInput('')
+     
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1> 
+
+      <div className="containerInput">
+      <input
+      type="text"
+      placeholder="Insira o CEP" 
+      value={input}
+      maxLength={8}
+      onChange={(e) =>setInput(e.target.value) }
+      />
+
+
+
+      <button className="button" onClick={handleSearch}>
+      <SlMagicWand size={22} color="#F2C572"/>
+      </button>
+
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+
+  <main className="main">
+  <h2>Cep: {cep.cep}</h2>
+  <span> {cep.logradouro}</span>
+  <span>Complemento: {cep.complemento}</span>
+  <span>{cep.bairro}</span>
+  <span>{cep.localidade} - {cep.uf}</span>
+
+
+</main>
+      )}
+
+
+
     </div>
   );
 }
